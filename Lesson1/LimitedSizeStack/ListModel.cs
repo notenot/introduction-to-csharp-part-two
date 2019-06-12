da56f4560b26d37e0b9669b34f8ff78a1a3b0ea3
notenot <notsu@mail.ru>
2019-06-12 23:58:52 +0300

    Complete task 'Undo'

--- Lesson1/LimitedSizeStack/ListModel.cs
+++ Lesson1/LimitedSizeStack/ListModel.cs
@@ -6,32 +6,48 @@ namespace TodoApplication
     public class ListModel<TItem>
     {
         public List<TItem> Items { get; }
-        public int Limit;
+        public int Limit { get; }
+        private LimitedSizeStack<Tuple<char, TItem, int>> Stack { get; }
 
         public ListModel(int limit)
         {
-            Items = new List<TItem>();
             Limit = limit;
+            Items = new List<TItem>();
+            Stack = new LimitedSizeStack<Tuple<char, TItem, int>>(Limit);
         }
 
         public void AddItem(TItem item)
         {
+            Stack.Push(new Tuple<char, TItem, int>('+', item, 0));
             Items.Add(item);
         }
 
         public void RemoveItem(int index)
         {
+            Stack.Push(new Tuple<char, TItem, int>('-', Items[index], index));
             Items.RemoveAt(index);
         }
 
         public bool CanUndo()
         {
-            return false;
+            return Stack.Count > 0;
         }
 
         public void Undo()
         {
-            throw new NotImplementedException();
+            if (!CanUndo())
+                return;
+
+            var lastAction = Stack.Pop();
+            switch (lastAction.Item1)
+            {
+                case '+':
+                    Items.Remove(lastAction.Item2);
+                    break;
+                case '-':
+                    Items.Insert(lastAction.Item3, lastAction.Item2);
+                    break;
+            }
         }
     }
 }
