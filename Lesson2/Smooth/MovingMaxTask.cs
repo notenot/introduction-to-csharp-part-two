39837fe546de55af64e3fedbc97fe72e1adf7f18
notenot <notsu@mail.ru>
2019-06-14 02:16:11 +0300

    Complete task 'Moving Max'

--- Lesson2/Smooth/MovingMaxTask.cs
+++ Lesson2/Smooth/MovingMaxTask.cs
@@ -1,16 +1,45 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace yield
 {
-
 	public static class MovingMaxTask
 	{
-		public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
-		{
-			//Fix me!
-			return data;
-		}
-	}
+        private class PotentialMaxValues
+        {
+            public double CurrentMax => deque.First.Value.Item2;
+
+            private int currentIndex;
+            private int maxLength;
+            private LinkedList<Tuple<int, double>> deque;
+
+            public PotentialMaxValues(int windowWidth)
+            {
+                currentIndex = 0;
+                maxLength = windowWidth;
+                deque = new LinkedList<Tuple<int, double>>();
+            }
+
+            public void Add(double value)
+            {
+                while (deque.Count != 0 && deque.Last.Value.Item2 <= value)
+                    deque.RemoveLast();
+                deque.AddLast(new Tuple<int, double>(currentIndex, value));
+                ++currentIndex;
+                while (deque.First.Value.Item1 < currentIndex - maxLength)
+                    deque.RemoveFirst();
+            }
+        }
+
+        public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
+        {
+            var values = new PotentialMaxValues(windowWidth);
+            foreach (var point in data)
+            {
+                values.Add(point.OriginalY);
+                point.MaxY = values.CurrentMax;
+                yield return point;
+            }
+        }
+    }
 }
