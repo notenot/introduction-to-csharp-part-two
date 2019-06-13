47a093f400f823194490cb55a6ae52c0ecaf298b
notenot <notsu@mail.ru>
2019-06-14 00:57:55 +0300

    Complete task 'Moving Average'

--- Lesson2/Smooth/MovingAverageTask.cs
+++ Lesson2/Smooth/MovingAverageTask.cs
@@ -6,8 +6,19 @@ namespace yield
 	{
 		public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
 		{
-			//Fix me!
-			return data;
-		}
+            var window = new Queue<double>();
+            var sum = 0.0;
+            foreach (var point in data)
+            {
+                if (window.Count == windowWidth)
+                    sum -= window.Dequeue();
+
+                sum += point.OriginalY;
+                window.Enqueue(point.OriginalY);
+                point.AvgSmoothedY = sum / window.Count;
+
+                yield return point;
+            }
+        }
 	}
 }
