7ce8d9b3d54d0a957654508b03351e6845c91177
notenot <notsu@mail.ru>
2019-06-18 16:41:37 +0300

    Complete task 'Ghosts'

--- Lesson3/Ghosts/GhostsTask.cs
+++ Lesson3/Ghosts/GhostsTask.cs
@@ -6,26 +6,56 @@ namespace hashes
 	public class GhostsTask : 
 		IFactory<Document>, IFactory<Vector>, IFactory<Segment>, IFactory<Cat>, IFactory<Robot>, 
 		IMagic
-	{
+    {
+        private readonly Document linkToDocument;
+        private readonly Vector linkToVector;
+        private readonly Segment linkToSegment;
+        private readonly Cat linkToCat;
+        private readonly Robot linkToRobot;
+
+        private readonly byte[] documentContent;
+
+        public GhostsTask()
+        {
+            documentContent = Encoding.UTF8.GetBytes("simple content");
+            linkToDocument = new Document(
+                "simple title", 
+                Encoding.UTF8,
+                documentContent);
+            linkToVector = new Vector(0.0, 0.0);
+            linkToSegment = new Segment(linkToVector, linkToVector);
+            linkToCat = new Cat(
+                "cat name", 
+                "cat breed", 
+                new DateTime(1995, 7, 8, 13, 13, 13));
+            linkToRobot = new Robot("simple id", 100.0);
+        }
+
 		public void DoMagic()
-		{
-		}
+        {
+            unchecked
+            {
+                for (var i = 0; i < documentContent.Length; ++i)
+                    ++documentContent[i];
+            }
+            linkToVector.Add(new Vector(1.0, 1.0));
+            linkToCat.Rename("another cat name");
+            Robot.BatteryCapacity /= 10;
+        }
 
-		// Чтобы класс одновременно реализовывал интерфейсы IFactory<A> и IFactory<B> 
-		// придется воспользоваться так называемой явной реализацией интерфейса.
-		// Чтобы отличать методы создания A и B у каждого метода Create нужно явно указать, к какому интерфейсу он относится.
-		// На самом деле такое вы уже видели, когда реализовывали IEnumerable<T>.
+        Vector IFactory<Vector>.Create() =>
+            linkToVector;
 
-		Vector IFactory<Vector>.Create()
-		{
-			throw new NotImplementedException();
-		}
+        Segment IFactory<Segment>.Create() =>
+            linkToSegment;
 
-		Segment IFactory<Segment>.Create()
-		{
-			throw new NotImplementedException();
-		}
+        Document IFactory<Document>.Create() =>
+            linkToDocument;
 
-		// И так даллее по аналогии...
-	}
+        Cat IFactory<Cat>.Create() =>
+            linkToCat;
+
+        Robot IFactory<Robot>.Create() =>
+            linkToRobot;
+    }
 }
