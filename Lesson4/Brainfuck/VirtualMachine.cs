e57c85c5a512f89fa9f0e57af9beb034bbf31d0d
Natalia Taranova <n.g.taranova@yandex.ru>
2019-06-20 18:08:24 +0300

    Complete task 'Brainfuck Virtual Machine'

--- Lesson4/Brainfuck/VirtualMachine.cs
+++ Lesson4/Brainfuck/VirtualMachine.cs
@@ -4,23 +4,40 @@ using System.Collections.Generic;
 namespace func.brainfuck
 {
 	public class VirtualMachine : IVirtualMachine
-	{
-		public VirtualMachine(string program, int memorySize)
-		{
-		}
+    {
+        public string Instructions { get; }
+        public int InstructionPointer { get; set; }
+        public byte[] Memory { get; }
+        public int MemoryPointer { get; set; }
+
+        private int memorySize;
+        private Dictionary<char, Action<IVirtualMachine>> actions;
+
+		public VirtualMachine(string program, int memorySize = 30000)
+        {
+            MemoryPointer = 0;
+            InstructionPointer = 0;
+            this.memorySize = memorySize;
+            Memory = new byte[memorySize];
+            Instructions = program;
+            actions = new Dictionary<char, Action<IVirtualMachine>>();
+        }
 
 		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
 		{
-			throw new NotImplementedException();
+            if (!actions.ContainsKey(symbol))
+			    actions.Add(symbol, execute);
 		}
 
-		public string Instructions { get; }
-		public int InstructionPointer { get; set; }
-		public byte[] Memory { get; }
-		public int MemoryPointer { get; set; }
 		public void Run()
-		{
-			throw new NotImplementedException();
-		}
+        {
+            while (InstructionPointer < Instructions.Length)
+            {
+                var command = Instructions[InstructionPointer];
+                if (actions.ContainsKey(command))
+                    actions[command](this);
+                ++InstructionPointer;
+            }
+        }
 	}
 }
