63f656dafafba92c2fd2a3eb54179fea521e6477
Natalia Taranova <n.g.taranova@yandex.ru>
2019-06-20 19:46:21 +0300

    Complete task 'Brainfuck Basic Commands'

--- Lesson4/Brainfuck/BrainfuckBasicCommands.cs
+++ Lesson4/Brainfuck/BrainfuckBasicCommands.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace func.brainfuck
 {
@@ -8,10 +6,38 @@ namespace func.brainfuck
 	{
 		public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
 		{
-			vm.RegisterCommand('.', b => { });
-			vm.RegisterCommand('+', b => {});
-			vm.RegisterCommand('-', b => {});
-			//...
-		}
+			vm.RegisterCommand('.', b => 
+                { write(Convert.ToChar(b.Memory[b.MemoryPointer])); });
+			vm.RegisterCommand('+', b => 
+                { unchecked { ++b.Memory[b.MemoryPointer]; } });
+            vm.RegisterCommand('-', b => 
+                { unchecked { --b.Memory[b.MemoryPointer]; } });
+            vm.RegisterCommand(',', b => 
+                { b.Memory[b.MemoryPointer] = Convert.ToByte(read()); });
+            vm.RegisterCommand('>', b => 
+                { b.MemoryPointer = (b.MemoryPointer + 1) % b.Memory.Length; });
+            vm.RegisterCommand('<', b => 
+                { b.MemoryPointer = (b.MemoryPointer + b.Memory.Length - 1) % b.Memory.Length; });
+            RegisterConstantCommands(vm);
+        }
+
+        private static void RegisterConstantCommands(IVirtualMachine vm)
+        {
+            for (var ch = 'a'; ch <= 'z'; ++ch)
+            {
+                var lowercase = ch;
+                vm.RegisterCommand(lowercase, b =>
+                    { b.Memory[b.MemoryPointer] = Convert.ToByte(lowercase); });
+                var uppercase = char.ToUpper(ch);
+                vm.RegisterCommand(uppercase, b =>
+                    { b.Memory[b.MemoryPointer] = Convert.ToByte(uppercase); });
+            }
+            for (var ch = '0'; ch <= '9'; ++ch)
+            {
+                var number = ch;
+                vm.RegisterCommand(number, b =>
+                    { b.Memory[b.MemoryPointer] = Convert.ToByte(number); });
+            }
+        }
 	}
 }
