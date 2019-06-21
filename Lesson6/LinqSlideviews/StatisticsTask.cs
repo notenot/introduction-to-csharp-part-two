768773aaab9276dff348c12c893e3e7949bd1486
Natalia Taranova <n.g.taranova@yandex.ru>
2019-06-21 23:45:31 +0300

    Complete task 'Slideviews Statistics'

--- Lesson6/LinqSlideviews/StatisticsTask.cs
+++ Lesson6/LinqSlideviews/StatisticsTask.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace linq_slideviews
@@ -8,7 +7,21 @@ namespace linq_slideviews
 	{
 		public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
 		{
-			throw new NotImplementedException();
-		}
+            var filteredVisits = visits
+                .OrderBy(record => record.UserId)
+                .ThenBy(record => record.DateTime)
+                .Bigrams()
+                .Where(tuple =>
+                    tuple.Item1.SlideType == slideType &&
+                    tuple.Item1.UserId == tuple.Item2.UserId &&
+                    tuple.Item1.SlideId != tuple.Item2.SlideId)
+                .Select(tuple => tuple.Item2.DateTime
+                    .Subtract(tuple.Item1.DateTime)
+                    .TotalMinutes)
+                .Where(minutes => 1.0 <= minutes && minutes <= 120.0)
+                .ToList();
+
+            return filteredVisits.Count == 0 ? 0.0 : filteredVisits.Median();
+        }
 	}
 }
