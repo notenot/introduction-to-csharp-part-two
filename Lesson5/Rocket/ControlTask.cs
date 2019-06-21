b0f5435cd25d995c46cdbff2698bb105929a5e5f
Natalia Taranova <n.g.taranova@yandex.ru>
2019-06-21 18:25:11 +0300

    Complete task 'Rocket Control'

--- Lesson5/Rocket/ControlTask.cs
+++ Lesson5/Rocket/ControlTask.cs
@@ -6,7 +6,14 @@ namespace func_rocket
 	{
 		public static Turn ControlRocket(Rocket rocket, Vector target)
 		{
-			return Turn.None;
-		}
+            var distance = target - rocket.Location;
+            var diffWithVelocity = distance.Angle - rocket.Velocity.Angle;
+            var diffWithDirection = distance.Angle - rocket.Direction;
+            var currentAngle = Math.Abs(diffWithVelocity) < 0.5 || Math.Abs(diffWithDirection) < 0.5
+                ? (diffWithDirection + diffWithVelocity) / 2 
+                : diffWithDirection;
+
+            return currentAngle < 0 ? Turn.Left : Turn.Right;
+        }
 	}
 }
