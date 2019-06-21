84916c8bd05147ad1d1039a378778ab427e1020c
Natalia Taranova <n.g.taranova@yandex.ru>
2019-06-21 22:29:18 +0300

    Complete task 'Slideviews Median and Bigrams'

--- Lesson6/LinqSlideviews/ExtensionsTask.cs
+++ Lesson6/LinqSlideviews/ExtensionsTask.cs
@@ -13,17 +13,36 @@ namespace linq_slideviews
 		/// </summary>
 		/// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
 		public static double Median(this IEnumerable<double> items)
-		{
-			throw new NotImplementedException();
-		}
+        {
+            var sortedItems = items.OrderBy(item => item).ToArray();
+            var length = sortedItems.Length;
+            if (length == 0)
+                throw new InvalidOperationException();
+
+            var middle = length / 2;
+            return length % 2 != 0
+                ? sortedItems[middle]
+                : (sortedItems[middle - 1] + sortedItems[middle]) / 2;
+        }
 
 		/// <returns>
 		/// Возвращает последовательность, состоящую из пар соседних элементов.
 		/// Например, по последовательности {1,2,3} метод должен вернуть две пары: (1,2) и (2,3).
 		/// </returns>
 		public static IEnumerable<Tuple<T, T>> Bigrams<T>(this IEnumerable<T> items)
-		{
-			throw new NotImplementedException();
-		}
+        {
+            using (var enumerator = items.GetEnumerator())
+            { 
+                if (!enumerator.MoveNext())
+                    yield break;
+
+                var previous = enumerator.Current;
+                while (enumerator.MoveNext())
+                {
+                    yield return Tuple.Create(previous, enumerator.Current);
+                    previous = enumerator.Current;
+                }
+            }
+        }
 	}
 }
