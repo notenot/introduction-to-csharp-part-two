ba34cc6e5cf2206b6265ff67710e04293b35906d
Natalia Taranova <n.g.taranova@yandex.ru>
2019-06-22 17:51:39 +0300

    Complete task 'Dungeon BFS'

--- Lesson7/Dungeon/BfsTask.cs
+++ Lesson7/Dungeon/BfsTask.cs
@@ -1,17 +1,42 @@
-using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dungeon
 {
 	public class BfsTask
 	{
-	    public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
+        public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
         {
-            yield break;
-		}
-	}
+            var visitedPoints = new HashSet<Point>();
+            var queue = new Queue<SinglyLinkedList<Point>>();
+            visitedPoints.Add(start);
+            queue.Enqueue(new SinglyLinkedList<Point>(start));
+            while (queue.Count != 0)
+            {
+                var currentPoint = queue.Dequeue();
+                var nextPoints = GetAdjacentPoints(currentPoint.Value)
+                    .Where(map.InBounds)
+                    .Where(point => map.Dungeon[point.X, point.Y] == MapCell.Empty)
+                    .Where(point => !visitedPoints.Contains(point));
+
+                foreach (var nextPoint in nextPoints)
+                {
+                    visitedPoints.Add(nextPoint);
+                    var pathPoint = new SinglyLinkedList<Point>(nextPoint, currentPoint);
+                    queue.Enqueue(pathPoint);
+                    if (chests.Contains(nextPoint))
+                        yield return pathPoint;
+                }
+            }
+        }
+
+        private static IEnumerable<Point> GetAdjacentPoints(Point point)
+        {
+            for (var dy = -1; dy <= 1; dy++)
+            for (var dx = -1; dx <= 1; dx++)
+                if (dx == 0 || dy == 0)
+                    yield return new Point { X = point.X + dx, Y = point.Y + dy };
+        }
+    }
 }
