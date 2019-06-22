64be510c5c4b535a21f86627de1b5fa84735e53a
Natalia Taranova <n.g.taranova@yandex.ru>
2019-06-22 23:34:53 +0300

    Init tasks 'Greedy' from eighth lesson

---
+++ Lesson8/Greedy/DijkstraPathFinder.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Greedy.Architecture;
+using System.Drawing;
+
+namespace Greedy
+{
+    public class DijkstraPathFinder
+    {
+        public IEnumerable<PathWithCost> GetPathsByDijkstra(State state, Point start,
+            IEnumerable<Point> targets)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
