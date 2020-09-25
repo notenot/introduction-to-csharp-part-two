6f5c73d4b7f866a0bb0971d2475ab69fdbba4b16
Natalia Taranova <n.g.taranova@yandex.ru>
2020-09-25 17:23:07 +0300

    Push last modifications

--- Lesson10/BinaryTrees/BinaryTree.cs
+++ Lesson10/BinaryTrees/BinaryTree.cs
@@ -1,10 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BinaryTrees
 {
-    
+    public class TreeNode<T> : IComparable
+        where T : IComparable
+    {
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+        public T Value { get; }
+
+        public TreeNode(T value, TreeNode<T> left, TreeNode<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+
+        public int CompareTo(TreeNode<T> other) => Value.CompareTo(other.Value);
+        public int CompareTo(object obj)
+        {
+            if (obj.GetType() != GetType())
+                throw new 
+        }
+    }
+
+    public class BinaryTree<T>
+    {
+        public TreeNode<T> Root { get; private set; }
+
+        public void Add(T value)
+        {
+            if (Root == null)
+            {
+                Root = new TreeNode<T>(value, null, null);
+                return;
+            }
+
+            while (true)
+            {
+                if (value.CompareTo(root.Value) < 0)
+                {
+                    root.LeftSize++;
+                    if (root.Left == null)
+                    {
+                        var tempNode = new Node(value);
+                        tempNode.Parent = root;
+                        root.Left = tempNode;
+                        Count++;
+                        return;
+                    }
+                    root = root.Left;
+                }
+                else
+                {
+                    if (root.Right == null)
+                    {
+                        var tempNode = new Node(value);
+                        tempNode.Parent = root;
+                        root.Right = tempNode;
+                        Count++;
+                        return;
+                    }
+                    root = root.Right;
+                }
+            }
+        }
+
+
+    }
 }
--- Lesson8/Greedy/NotGreedyPathFinder.cs
+++ Lesson8/Greedy/NotGreedyPathFinder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using Greedy.Architecture;
 using Greedy.Architecture.Drawing;
@@ -9,7 +10,38 @@ namespace Greedy
 	{
 		public List<Point> FindPathToCompleteGoal(State state)
 		{
-			return new List<Point>();
+            // var chestList = state.Chests
+            MakePermutations(new int[state.Chests.Count], 0);
+            return new List<Point>();
 		}
-	}
+
+        private static void Evaluate(int[] permutation)
+        {
+            int price = 0;
+            for (int i = 0; i < permutation.Length; i++)
+                price += prices[permutation[i], permutation[(i + 1) % permutation.Length]];
+            foreach (var e in permutation)
+                Console.Write(e + " ");
+            Console.Write(price);
+            Console.WriteLine();
+        }
+
+        private static void MakePermutations(int[] permutation, int position)
+        {
+            if (position == permutation.Length)
+            {
+                Evaluate(permutation);
+                return;
+            }
+
+            for (var i = 0; i < permutation.Length; i++)
+            {
+                var index = Array.IndexOf(permutation, i, 0, position);
+                if (index != -1)
+                    continue;
+                permutation[position] = i;
+                MakePermutations(permutation, position + 1);
+            }
+        }
+    }
 }
